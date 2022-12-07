b31cfa349f63f4015cfa1bcad01931759da350ca
Simon Dunn <simondunn90@gmail.com>
2022-12-07 07:59:56 +0000

    set fullDayMinutes

--- calculate-flexi.js
+++ calculate-flexi.js
@@ -1,6 +1,6 @@
 
 const calculateFlexi = (totalMinutes) => {
-  const fullDayMinutes = Math.floor(7.36 * 60)
+  const fullDayMinutes = 444 // 7 hours 24 minutes
   const minutes = Math.floor(totalMinutes - fullDayMinutes)
   let message
   if (fullDayMinutes <= totalMinutes) {
